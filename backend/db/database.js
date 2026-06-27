556609262e06bd29147a63efcb203c6c25d04e54
Prachi601 <prachibahad92@gmail.com>
2026-06-27 14:53:13 +0530

    Connected backend to Railway MySQL

--- backend/db/database.js
+++ backend/db/database.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "autorack.proxy.rlwy.net",
   port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "chess_master",
+  user: process.env.DB_USER || "29481",
+  password: process.env.DB_PASSWORD || "OwurzynhBBhwkyvfyalFxPCJNInVQLYS",
+  database: process.env.DB_NAME || "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
